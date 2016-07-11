66c15478630dbfe39c0a39cc8fa226c297fd64d2
JeremyTanCD <jeremytanchongdao@gmail.com>
2016-07-11 17:43:38 +0800

    Made changes to Users table
    - Added default constraints for LockoutEndDateUTC and AccessFailedCount.

--- Database/dbo/Tables/Users.sql
+++ Database/dbo/Tables/Users.sql
@@ -6,8 +6,8 @@
     [Email]             NVARCHAR (256)   NULL,
 	[EmailConfirmed]	BIT NOT NULL,
 	[TwoFactorEnabled]  BIT NOT NULL,
-	[LockoutEndDateUTC] DATETIME2         NOT NULL,
+	[LockoutEndDateUTC] DATETIME2 CONSTRAINT [DF_dbo_Users_LockoutEndDateUTC] DEFAULT NULL NULL,
     [LockoutEnabled]    BIT         NOT NULL,
-    [AccessFailedCount] INT              NOT NULL,
+    [AccessFailedCount] INT CONSTRAINT [DF_dbo_Users_AccessFailedCount] DEFAULT 0 NOT NULL,
     CONSTRAINT [PK_dbo_Users] PRIMARY KEY CLUSTERED ([UserID] ASC)
 );
