4a25b62eef8a1f73bc3aca7056349d7d3e90fad5
JeremyTan <jeremytanchongdao@gmail.com>
2016-07-20 11:24:16 +0800

    Made changes to InsertMember stored procedure
    - No longer takes columns with default values as arguments.

--- Database/Website/Stored Procedures/InsertMember.sql
+++ Database/Website/Stored Procedures/InsertMember.sql
@@ -1,17 +1,11 @@
 ﻿CREATE PROCEDURE [Website].[InsertMember]
-	@Id INT,
-	@Username NVARCHAR(256), 
 	@PasswordHash NVARCHAR(MAX),  
 	@SecurityStamp NVARCHAR(MAX), 
 	@Email NVARCHAR(256),
-    @EmailConfirmed BIT, 
-	@TwoFactorEnabled BIT, 
-	@LockoutEnabled BIT,
-	@LockoutEndDateUtc DATETIME2,
-	@AccessFailedCount INT
+	@NormalizedEmail NVARCHAR(256)
 AS
 
 INSERT INTO [dbo].[Members]
-	(Username, PasswordHash, SecurityStamp, Email, EmailConfirmed, TwoFactorEnabled, LockoutEnabled)
+	(PasswordHash, SecurityStamp, Email, NormalizedEmail)
     OUTPUT INSERTED.[MemberId]
-    VALUES  (@Username, @PasswordHash, @SecurityStamp, @Email, @EmailConfirmed, @TwoFactorEnabled, @LockoutEnabled);
+    VALUES  (@PasswordHash, @SecurityStamp, @Email, @NormalizedEmail);
