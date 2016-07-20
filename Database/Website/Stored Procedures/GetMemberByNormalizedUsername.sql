069d22999e02813c699f01d8986c22cf80272d45
JeremyTan <jeremytanchongdao@gmail.com>
2016-07-20 11:25:02 +0800

    Made changes to and renamed GetMemberByUsername stored procedure
    - Renamed to GetMemberByNormalizedUsername.

---
+++ Database/Website/Stored Procedures/GetMemberByNormalizedUsername.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [Website].[GetMemberByNormalizedUsername]
+	@NormalizedUsername NVARCHAR(256)
+AS
+BEGIN
+	SELECT *
+	FROM [dbo].[Members] 
+	WHERE [NormalizedUsername]=@NormalizedUsername  
+END
+
--- Database/Website/Stored Procedures/GetMemberByUsername.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE [Website].[GetMemberByUsername]
-	@Username NVARCHAR(256)
-AS
-BEGIN
-	SELECT [Username], [PasswordHash], [SecurityStamp], [Email], [EmailConfirmed], [TwoFactorEnabled], [LockoutEnabled], [MemberId] AS Id 
-	FROM [dbo].[Members] 
-	WHERE [Username]=@Username  
-END
-
