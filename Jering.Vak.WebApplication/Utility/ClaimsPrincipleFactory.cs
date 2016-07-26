2553e34d1041a8d3b8fe691bd4d1da6171277bf9
JeremyTan <jeremytanchongdao@gmail.com>
2016-07-26 21:38:32 +0800

    Made changes to solutions
    - Added Jering.Vak.WebApplication and Jering.Vak.WebApplication.Test projects.

---
+++ Jering.Vak.WebApplication/Utility/ClaimsPrincipleFactory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using Jering.Vak.DatabaseInterface;
+using System.Collections.Generic;
+
+namespace Jering.Vak.WebApplication.Utility
+{
+    /// <summary>
+    /// Provides methods to create a claims principal for a given member.
+    /// </summary>
+    public class ClaimsPrincipalFactory
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClaimsPrincipalFactory"/> class.
+        /// </summary>
+        /// <param name="memberRepository">The <see cref="MemberRepository"/> to retrieve user information from.</param>
+        /// <param name="roleManager">The <see cref="RoleRepository"/> to retrieve a user's roles from.</param>
+        /// <param name="identityOptionsAccessor">The configured <see cref="Builder.IdentityOptions"/>.</param>
+        public ClaimsPrincipalFactory(MemberRepository memberRepository, RoleRepository roleRepository, IOptions<IdentityOptions> identityOptionsAccessor)
+        {
+            _memberRepository = memberRepository;
+            _roleRepository = roleRepository;
+            _identityOptions = identityOptionsAccessor.Value;
+        }
+
+        private MemberRepository _memberRepository { get; }
+        private RoleRepository _roleRepository { get; }
+        private IdentityOptions _identityOptions { get; }
+
+        /// <summary>
+        /// Creates a <see cref="ClaimsPrincipal"/> from a member asynchronously.
+        /// </summary>
+        /// <param name="user">The member to create a <see cref="ClaimsPrincipal"/> from.</param>
+        /// <returns>The <see cref="Task"/> that represents the asynchronous creation operation, containing the created <see cref="ClaimsPrincipal"/>.</returns>
+        public async Task<ClaimsPrincipal> CreateAsync(Member member)
+        {
+            var claimsIdentity = new ClaimsIdentity(
+                _identityOptions.Cookies.ApplicationCookieAuthenticationScheme,
+                _identityOptions.ClaimsIdentity.UserNameClaimType,
+                _identityOptions.ClaimsIdentity.RoleClaimType);
+
+            //claimsIdentity.AddClaim(new Jering.Vak.DatabaseInterface.Claim(_identityOptions.ClaimsIdentity.UserIdClaimType, member.MemberId.ToString()));
+            //claimsIdentity.AddClaim(new Claim(_identityOptions.ClaimsIdentity.UserNameClaimType, member.Email));
+            //claimsIdentity.AddClaim(new Claim(_identityOptions.ClaimsIdentity.SecurityStampClaimType, member.SecurityStamp));
+
+            IEnumerable<Role> roles = await _memberRepository.GetMemberRolesAsync(member.MemberId);
+            foreach (Role role in roles)
+            {
+                //claimsIdentity.AddClaim(new Claim(_identityOptions.ClaimsIdentity.RoleClaimType, role.Name));
+                claimsIdentity.AddClaims(await _roleRepository.GetRoleClaimsAsync(role.RoleId));
+            }
+
+            claimsIdentity.AddClaims(await _memberRepository.GetMemberClaimsAsync(member.MemberId));
+
+            return new ClaimsPrincipal(claimsIdentity);
+        }
+    }
+}
---
+++ Jering.Vak.WebApplication/Utility/SignInManager.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Jering.Vak.DatabaseInterface;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Jering.Vak.WebApplication.Utility
+{
+    public class SignInManager
+    {
+        public SignInManager(ClaimsPrincipalFactory claimsPrincipalFactory, 
+            IHttpContextAccessor httpContextAccessor,
+            IOptions<IdentityOptions> identityOptionsAccessor)
+        {
+            _claimsPrincipalFactory = claimsPrincipalFactory;
+            _httpContext = httpContextAccessor.HttpContext;
+            _identityOptions = identityOptionsAccessor.Value;
+        }
+
+        private ClaimsPrincipalFactory _claimsPrincipalFactory { get; }
+        private HttpContext _httpContext { get; }
+        private IdentityOptions _identityOptions { get; }
+
+        public async Task SignInAsync(Member member, AuthenticationProperties authenticationProperties)
+        {
+            var userPrincipal = await _claimsPrincipalFactory.CreateAsync(member);
+
+            //if (authenticationMethod != null)
+            //{
+                //userPrincipal.Identities.First().AddClaim(new Claim(ClaimTypes.AuthenticationMethod, authenticationMethod));
+            //}
+            await _httpContext.Authentication.SignInAsync(
+                _identityOptions.Cookies.ApplicationCookieAuthenticationScheme,
+                userPrincipal,
+                authenticationProperties);
+        }
+    }
+}
