ecc83dd871133ff5c9e31d94d41d9afcc41231c1
JeremyTan <jeremytanchongdao@gmail.com>
2016-07-27 11:26:52 +0800

    Made changes to database
    - Added error handling for AddAccountClaim and AddAccountRole stored procedures.

--- Jering.Vak.Database/Website/Stored Procedures/Members/AddAccountClaim.sql
+++ Jering.Vak.Database/Website/Stored Procedures/Members/AddAccountClaim.sql
@@ -6,6 +6,20 @@ BEGIN
 	SET NOCOUNT ON;
 	SET XACT_ABORT ON;
 
-	INSERT INTO [dbo].[AccountClaims] ([AccountId], [ClaimId])
-	VALUES (@AccountId, @ClaimId)
+	BEGIN TRY
+		INSERT INTO [dbo].[AccountClaims] ([AccountId], [ClaimId])
+		VALUES (@AccountId, @ClaimId)
+	END TRY
+    BEGIN CATCH
+        IF XACT_STATE() <> 0 ROLLBACK;
+		DECLARE @errorNumber INT = ERROR_NUMBER();
+		DECLARE @errorMessage NVARCHAR(MAX);
+
+		IF @errorNumber = 2627 
+			THROW 51000, N'Account already has claim.', 1;
+		ELSE IF @errorNumber = 547
+			THROW 51000, N'Account or claim does not exist.', 1;
+		ELSE
+			THROW 52000, N'An unexpected error occurred.', 1;
+    END CATCH;
 END
--- Jering.Vak.Database/Website/Stored Procedures/Members/AddAccountRole.sql
+++ Jering.Vak.Database/Website/Stored Procedures/Members/AddAccountRole.sql
@@ -3,6 +3,23 @@
 	@RoleId INT
 AS
 BEGIN
-	INSERT INTO [dbo].[AccountRoles] ([AccountId], [RoleId])
-	VALUES (@AccountId, @RoleId)
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	BEGIN TRY
+		INSERT INTO [dbo].[AccountRoles] ([AccountId], [RoleId])
+		VALUES (@AccountId, @RoleId)
+	END TRY
+    BEGIN CATCH
+        IF XACT_STATE() <> 0 ROLLBACK;
+		DECLARE @errorNumber INT = ERROR_NUMBER();
+		DECLARE @errorMessage NVARCHAR(MAX);
+
+		IF @errorNumber = 2627 
+			THROW 51000, N'Account already has role.', 1;
+		ELSE IF @errorNumber = 547
+			THROW 51000, N'Account or role does not exist.', 1;
+		ELSE
+			THROW 52000, N'An unexpected error occurred.', 1;
+    END CATCH;
 END
