c31c573510ee5afc784f2759a19d795e0c8ad817
JeremyTan <jeremytanchongdao@gmail.com>
2016-08-10 18:06:54 +0800

    Added project Jering.AccountManagement.Extensions
    - Contains classes used to setup account management.

---
+++ Jering.AccountManagement.Extensions/AccountManagementBuilder.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jering.AccountManagement.DatabaseInterface;
+using Jering.AccountManagement.Security;
+
+namespace Jering.AccountManagement.Extensions
+{
+    public class AccountManagementBuilder
+    {
+        /// <summary>
+        /// Gets the <see cref="IServiceCollection"/> services are attached to.
+        /// </summary>
+        /// <value>
+        /// The <see cref="IServiceCollection"/> services are attached to.
+        /// </value>
+        private IServiceCollection _serviceCollection { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Type _userType { get; }
+
+        public AccountManagementBuilder(Type userType, IServiceCollection serviceCollection)
+        {
+            _userType = userType;
+            _serviceCollection = serviceCollection;
+        }
+
+        /// <summary>
+        /// Adds an <see cref="IAccountRepository{TAccount}"/> for the <seealso cref="_userType"/>.
+        /// </summary>
+        /// <typeparam name="TAccountRepository">A type that implements <see cref="IAccountRepository{TAccount}"/>.</typeparam>
+        /// <returns>The current <see cref="AccountManagementBuilder"/> instance.</returns>
+        public virtual AccountManagementBuilder AddAccountRepository<TAccountRepository>() where TAccountRepository : class
+        {
+            return AddScoped(typeof(IAccountRepository<>).MakeGenericType(_userType), typeof(TAccountRepository));
+        }
+
+        public virtual AccountManagementBuilder AddTokenService<TTokenServiceType>(string tokenServiceName)
+        {
+            AddTokenService(tokenServiceName, typeof(TTokenServiceType));
+            return this;
+        }
+
+        private AccountManagementBuilder AddTokenService(string tokenServiceName, Type tokenServiceType)
+        {
+            _serviceCollection.Configure<AccountSecurityOptions>(options =>
+            {
+                options.TokenServiceOptions.TokenServiceMap[tokenServiceName] = tokenServiceType;
+            });
+
+            AddScoped(tokenServiceType, tokenServiceType);
+            return this;
+        }
+
+        public virtual AccountManagementBuilder AddDefaultTokenServices()
+        {
+            Type dataProtectionTokenServiceType = typeof(DataProtectionTokenService<>).MakeGenericType(_userType);
+            Type totpTokenServiceType = typeof(TotpTokenService<>).MakeGenericType(_userType);
+
+            return AddTokenService(TokenServiceOptions.DataProtectionTokenService, dataProtectionTokenServiceType).
+                AddTokenService(TokenServiceOptions.TotpTokenService, totpTokenServiceType);
+        }
+
+        private AccountManagementBuilder AddScoped(Type serviceType, Type concreteType)
+        {
+            _serviceCollection.AddScoped(serviceType, concreteType);
+            return this;
+        }
+    }
+}
---
+++ Jering.AccountManagement.Extensions/AccountManagementServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Jering.AccountManagement.DatabaseInterface;
+using Jering.AccountManagement.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jering.AccountManagement.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AccountManagementServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TAccount"></typeparam>
+        /// <param name="services"></param>
+        public static AccountManagementBuilder AddAccountManagement<TAccount>(this IServiceCollection services, IConfigurationRoot configurationRoot) where TAccount : IAccount
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<ClaimsPrincipalFactory<TAccount>, ClaimsPrincipalFactory<TAccount>>();
+            services.AddScoped<AccountSecurityServices<TAccount>, AccountSecurityServices<TAccount>>();
+
+            return new AccountManagementBuilder(typeof(TAccount), services);
+        }
+    }
+}
