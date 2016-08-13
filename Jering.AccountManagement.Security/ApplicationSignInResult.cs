ed6d158e4f0b6fbf25b9c3676bf5e04fc06ba706
JeremyTan <jeremytanchongdao@gmail.com>
2016-08-13 21:29:28 +0800

    Made changes to Jering.AccountManagement.Security
    - Added Application and TwoFactor sign in result enums.

---
+++ Jering.AccountManagement.Security/ApplicationSignInResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jering.AccountManagement.Security
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ApplicationSignInResult
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Succeeded,
+        /// <summary>
+        /// 
+        /// </summary>
+        TwoFactorRequired,
+        /// <summary>
+        /// 
+        /// </summary>
+        Failed
+    }
+}
---
+++ Jering.AccountManagement.Security/TwoFactorSignInResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jering.AccountManagement.Security
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum TwoFactorSignInResult
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Succeeded,
+        /// <summary>
+        /// 
+        /// </summary>
+        Failed
+    }
+}
