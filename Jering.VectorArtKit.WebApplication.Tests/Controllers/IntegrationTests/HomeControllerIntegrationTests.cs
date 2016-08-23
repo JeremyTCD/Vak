9bc1d9f5b6ec7a8131c10d7ca63a0084097c7107
JeremyTan <jeremytanchongdao@gmail.com>
2016-08-23 17:50:42 +0800

    Made changes to HomeController
    - Added SetSignedInAccountFilter for Index action.
    - Added tests for Index action.

---
+++ Jering.VectorArtKit.WebApplication.Tests/Controllers/IntegrationTests/HomeControllerIntegrationTests.cs
@@ -0,0 +1,77 @@
+﻿using Jering.VectorArtKit.WebApplication.BusinessModel;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jering.VectorArtKit.WebApplication.Tests.Controllers.IntegrationTests
+{
+    [Collection("ControllersCollection")]
+    public class HomeControllerIntegrationTests
+    {
+        private HttpClient _httpClient { get; }
+        private VakAccountRepository vakAccountRepository { get; }
+        private Func<Task> _resetAccountsTable { get; }
+        public HomeControllerIntegrationTests(ControllersFixture controllersFixture)
+        {
+            _httpClient = controllersFixture.HttpClient;
+            vakAccountRepository = controllersFixture.VakAccountRepository;
+            _resetAccountsTable = controllersFixture.ResetAccountsTable;
+        }
+
+        [Fact]
+        public async Task IndexGet_ReturnsHomeIndexViewWithLogOffFormWhenAccountIsLoggedIn()
+        {
+            //Arrange
+            await _resetAccountsTable();
+            await vakAccountRepository.CreateAccountAsync("Email1@test.com", "Password1@");
+            await vakAccountRepository.UpdateAccountEmailConfirmedAsync(1);
+
+            HttpResponseMessage loginGetResponse = await _httpClient.GetAsync("Account/Login");
+
+            IDictionary<string, string> formPostBodyData = new Dictionary<string, string>
+            {
+                { "Email", "Email1@test.com"},
+                { "Password", "Password1@"},
+                { "RememberMe", "true" },
+                { "__RequestVerificationToken", await AntiForgeryTokenHelper.ExtractAntiForgeryToken(loginGetResponse) }
+            };
+            HttpRequestMessage loginPostRequest = RequestHelper.CreateWithCookiesFromResponse("Account/Login", HttpMethod.Post, formPostBodyData, loginGetResponse);
+            HttpResponseMessage loginPostResponse = await _httpClient.SendAsync(loginPostRequest);
+
+            HttpRequestMessage indexGetRequest = RequestHelper.CreateWithCookiesFromResponse("Home/Index", HttpMethod.Get, null, loginPostResponse);
+
+            // Act
+            HttpResponseMessage indexGetResponse = await _httpClient.SendAsync(indexGetRequest);
+
+            // Assert
+            IDictionary<string, string> cookies = CookiesHelper.ExtractCookiesFromResponse(indexGetResponse);
+            string html = await indexGetResponse.Content.ReadAsStringAsync();
+
+            Assert.Equal("OK", indexGetResponse.StatusCode.ToString());
+            Assert.Contains("Email1@test.com", html);
+            Assert.Contains("LogOff", html);
+        }
+
+        [Fact]
+        public async Task IndexGet_ReturnsHomeIndexViewWithLoginAndRegisterButtonsWhenAccountIsNotLoggedIn()
+        {
+            // Act
+            HttpResponseMessage indexGetResponse = await _httpClient.GetAsync("Home/Index");
+
+            // Assert
+            IDictionary<string, string> cookies = CookiesHelper.ExtractCookiesFromResponse(indexGetResponse);
+            string html = await indexGetResponse.Content.ReadAsStringAsync();
+
+            Assert.Equal("OK", indexGetResponse.StatusCode.ToString());
+            Assert.Contains("Register", html);
+            Assert.Contains("Login", html);
+        }
+    }
+}
+
+
--- Jering.VectorArtKit.WebApplication/Controllers/HomeController.cs
+++ Jering.VectorArtKit.WebApplication/Controllers/HomeController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Jering.VectorArtKit.WebApplication.Filters;
+using Jering.VectorArtKit.WebApplication.ViewModels.Shared;
 
 namespace Jering.VectorArtKit.WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Get: /Home/Index
+        /// </summary>
+        /// <returns>
+        /// Home index view with logoff form, when an account is logged in.
+        /// Home index view with login form, when no account is logged in.
+        /// </returns>
+        [ServiceFilter(typeof(SetSignedInAccountFilter))]
         public IActionResult Index()
         {
             return View();
