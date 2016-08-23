978e390e979beb3055c9f56edf8ded96d8ec1a8c
JeremyTan <jeremytanchongdao@gmail.com>
2016-08-23 17:51:24 +0800

    Added file SetSignedInAccountFilter

---
+++ Jering.VectorArtKit.WebApplication/Filters/SetSignedInAccountFilter.cs
@@ -0,0 +1,31 @@
+﻿using Jering.AccountManagement.Security;
+using Jering.VectorArtKit.WebApplication.BusinessModel;
+using Jering.VectorArtKit.WebApplication.ViewModels.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Security.Claims;
+
+namespace Jering.VectorArtKit.WebApplication.Filters
+{
+    public class SetSignedInAccountFilter : IActionFilter
+    {
+        private IAccountSecurityServices<VakAccount> _accountSecurityServices { get; set; }
+
+        public SetSignedInAccountFilter(IAccountSecurityServices<VakAccount> accountSecurityServices)
+        {
+            _accountSecurityServices = accountSecurityServices;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            (context.Controller as Controller).ViewData.Add(
+                nameof(VakAccount),
+                _accountSecurityServices.GetSignedInAccount());
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+    }
+}
