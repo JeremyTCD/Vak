70edbc16c27534603212d18cbf3de62a43933f1d
JeremyTan <jeremytanchongdao@gmail.com>
2016-09-03 18:04:09 +0800

    Added Email property to ConfirmEmailViewModel

--- Jering.VectorArtKit.WebApplication/ViewModels/Account/ConfirmEmailViewModel.cs
+++ Jering.VectorArtKit.WebApplication/ViewModels/Account/ConfirmEmailViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Jering.DataAnnotations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,11 @@ namespace Jering.VectorArtKit.WebApplication.ViewModels.Account
 {
     public class ConfirmEmailViewModel
     {
+        [Required]
         public string Token { get; set; }
+
+        [Required]
+        [ValidateEmailAddress]
+        public string Email { get; set; }
     }
 }
