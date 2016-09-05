57935456952ac5fc634eb007deb60f4440c7a12a
JeremyTan <jeremytanchongdao@gmail.com>
2016-09-05 18:11:55 +0800

    Made changes to Accounts table
    - Added AlternativeEmail and AlternativeEmailVerified columns.
    - Renamed DisplayName and EmailVerified columns.

--- Jering.AccountManagement.Database/dbo/Tables/Accounts.sql
+++ Jering.AccountManagement.Database/dbo/Tables/Accounts.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [dbo].[Accounts] (
-    [AccountId]		  	 INT IDENTITY(1,1) NOT NULL,
-    [Username]		 	 NVARCHAR (256) CONSTRAINT [DF_dbo_Accounts_Username] DEFAULT NULL NULL,
-	[SecurityStamp]      UNIQUEIDENTIFIER NOT NULL,
-    [PasswordHash]		 BINARY(32) NOT NULL,
-	[Email]				 NVARCHAR (256)   NOT NULL,
-	[EmailConfirmed]	 BIT CONSTRAINT [DF_dbo_Accounts_EmailConfirmed] DEFAULT 0 NOT NULL,
-	[TwoFactorEnabled]   BIT CONSTRAINT [DF_dbo_Accounts_TwoFactorEnabled] DEFAULT 0 NOT NULL,
+    [AccountId]		  	        INT IDENTITY(1,1) NOT NULL,
+    [DisplayName]		        NVARCHAR (256) CONSTRAINT [DF_dbo_Accounts_DisplayName] DEFAULT NULL NULL,
+	[SecurityStamp]             UNIQUEIDENTIFIER NOT NULL,
+    [PasswordHash]		        BINARY(32) NOT NULL,
+	[Email]				        NVARCHAR (256)   NOT NULL,
+	[EmailVerified]	            BIT CONSTRAINT [DF_dbo_Accounts_EmailVerified] DEFAULT 0 NOT NULL,
+	[AlternativeEmail]			NVARCHAR (256)   CONSTRAINT [DF_dbo_Accounts_AlternativeEmail] DEFAULT NULL NULL,
+	[AlternativeEmailVerified]	BIT CONSTRAINT [DF_dbo_Accounts_AlternativeEmailVerified] DEFAULT 0 NOT NULL,
+	[TwoFactorEnabled]          BIT CONSTRAINT [DF_dbo_Accounts_TwoFactorEnabled] DEFAULT 0 NOT NULL,
     CONSTRAINT [PK_dbo_Accounts] PRIMARY KEY CLUSTERED ([AccountId] ASC),
 	CONSTRAINT [UQ_dbo_Accounts_Email] UNIQUE ([Email])
 );
