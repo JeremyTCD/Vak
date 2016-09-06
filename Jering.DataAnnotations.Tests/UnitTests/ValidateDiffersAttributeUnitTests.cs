57b4f22e98628bf29e48598c2ece84eb8d20c710
JeremyTan <jeremytanchongdao@gmail.com>
2016-09-06 14:37:39 +0800

    Added ValidateDiffersAttribute

---
+++ Jering.DataAnnotations.Tests/UnitTests/ValidateDiffersAttributeUnitTests.cs
@@ -0,0 +1,76 @@
+﻿
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Jering.DataAnnotations.Tests.UnitTests
+{
+    public class ValidateDiffersAttributeUnitTests
+    {
+        [Theory]
+        [MemberData(nameof(IsValidData))]
+        public void IsValid_GetValidationResult_ReturnsCorrectValidationResults(object model, object actualValue)
+        {
+            // Arrange
+            DummyOptions dummyOptions = new DummyOptions();
+
+            Mock<IOptions<DummyOptions>> mockOptions = new Mock<IOptions<DummyOptions>>();
+            mockOptions.Setup(o => o.Value).Returns(new DummyOptions());
+
+            Mock<IServiceProvider> mockServiceProvider = new Mock<IServiceProvider>();
+            mockServiceProvider.Setup(s => s.GetService(typeof(IOptions<DummyOptions>))).Returns(mockOptions.Object);
+
+            ValidationContext validationContext = new ValidationContext(model, mockServiceProvider.Object, null);
+
+            ValidateDiffersAttribute validateDiffersAttribute = new ValidateDiffersAttribute("ExpectedValue", nameof(dummyOptions.dummyString), typeof(DummyOptions));
+
+            // Act
+            // IsValid is a protected function, the public function GetValidationResult calls it.
+            ValidationResult validationResult = validateDiffersAttribute.GetValidationResult(actualValue, validationContext);
+
+            // Assert
+            DummyModel dummyModel = model as DummyModel;
+            if (dummyModel != null && dummyModel.ExpectedValue != actualValue as string)
+            {
+                Assert.Null(validationResult);
+            }
+            else
+            {
+                Assert.Equal(dummyOptions.dummyString, validationResult.ErrorMessage);
+            }
+        }
+
+        public static IEnumerable<object[]> IsValidData()
+        {
+            yield return new object[] { new DummyModelEmpty(), "" };
+            yield return new object[] { new DummyModel { ExpectedValue = "differs" }, 0 };
+            yield return new object[] { new DummyModel { ExpectedValue = "does not differ" }, "does not differ" };
+        }
+
+        private class DummyModelEmpty : IXunitSerializable
+        {
+            public void Deserialize(IXunitSerializationInfo info) { }
+
+            public void Serialize(IXunitSerializationInfo info) { }
+        }
+
+        private class DummyModel : IXunitSerializable
+        {
+            public string ExpectedValue { get; set; }
+
+            public void Deserialize(IXunitSerializationInfo info)
+            {
+                ExpectedValue = info.GetValue<string>("ExpectedValue");
+            }
+
+            public void Serialize(IXunitSerializationInfo info)
+            {
+                info.AddValue("ExpectedValue", ExpectedValue);
+            }
+        }
+    }
+}
---
+++ Jering.DataAnnotations/ValidateDiffersAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Jering.DataAnnotations
+{
+    /// <summary>
+    /// Validates differing properties.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class ValidateDiffersAttribute : OptionsValidationAttribute
+    {
+        private string _otherProperty { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="otherProperty"></param>
+        public ValidateDiffersAttribute(string otherProperty):this(otherProperty, null, null)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="otherProperty"></param>
+        /// <param name="errorMessageProperty"></param>
+        /// <param name="optionsType"></param>
+        public ValidateDiffersAttribute(string otherProperty, string errorMessageProperty, Type optionsType):base(errorMessageProperty, optionsType)
+        {
+            _otherProperty = otherProperty;
+        }
+
+        /// <summary>
+        /// Validates <paramref name="value"/>. 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="validationContext"></param>
+        /// <returns>
+        /// <see cref="ValidationResult"/> with an error message if other property does not exist.
+        /// <see cref="ValidationResult"/> with an error message if <paramref name="value"/> does not differ from other property.
+        /// <see cref="ValidationResult.Success"/> if <paramref name="value"/> differs from other property.
+        /// </returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            PropertyInfo otherPropertyInfo = validationContext.ObjectType.GetRuntimeProperty(_otherProperty);
+            if (otherPropertyInfo == null)
+            {
+                return new ValidationResult(GetErrorMessage(validationContext));
+            }
+
+            object otherPropertyValue = otherPropertyInfo.GetValue(validationContext.ObjectInstance, null);
+            if (Equals(value, otherPropertyValue))
+            {
+                return new ValidationResult(GetErrorMessage(validationContext));
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
