b57b2ea20a901e63cd8b04ad8781cd8ebb96cdac
JeremyTan <jeremytanchongdao@gmail.com>
2016-09-06 12:07:38 +0800

    Added IsPersistenClaimType property to ClaimOptions

--- Jering.AccountManagement.Security/ClaimOptions.cs
+++ Jering.AccountManagement.Security/ClaimOptions.cs
@@ -12,35 +12,28 @@ namespace Jering.AccountManagement.Security
     public class ClaimOptions
     {
         /// <summary>
-        /// Gets or sets the ClaimType used for a Role claim.
+        /// 
         /// </summary>
-        /// <remarks>
-        /// This defaults to <see cref="ClaimTypes.Role"/>.
-        /// </remarks>
         public string RoleClaimType { get; set; } = ClaimTypes.Role;
 
         /// <summary>
-        /// Gets or sets the ClaimType used for the user name claim.
+        /// 
         /// </summary>
-        /// <remarks>
-        /// This defaults to <see cref="ClaimTypes.Name"/>.
-        /// </remarks>
         public string UsernameClaimType { get; set; } = ClaimTypes.Name;
 
         /// <summary>
-        /// Gets or sets the ClaimType used for the user identifier claim.
+        /// 
         /// </summary>
-        /// <remarks>
-        /// This defaults to <see cref="ClaimTypes.NameIdentifier"/>.
-        /// </remarks>
         public string AccountIdClaimType { get; set; } = ClaimTypes.NameIdentifier;
 
         /// <summary>
-        /// Gets or sets the ClaimType used for the security stamp claim..
+        /// 
         /// </summary>
-        /// <remarks>
-        /// This defaults to "AspNet.Identity.SecurityStamp".
-        /// </remarks>
+        public string IsPersistenClaimType { get; set; } = ClaimTypes.IsPersistent;
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string SecurityStampClaimType { get; set; } = "Jering.VectorArtKit.Account.SecurityStamp";
     }
 }
