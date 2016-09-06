a0f59877b2d98e1af2e65caba2b9a5eb4aac4a93
JeremyTan <jeremytanchongdao@gmail.com>
2016-09-06 21:47:09 +0800

    Added ElapsedDurationTagHelper
    - Added TimeSpanExtensions and unit tests for use in new tag helper.
    - Added taghelper to views via _viewimports.
    - Added elapsed duration span to manageaccount.

---
+++ Jering.VectorArtKit.WebApplication/TagHelpers/ElapsedDurationTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Jering.VectorArtKit.WebApplication.Extensions;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Jering.VectorArtKit.WebApplication.TagHelpers
+{
+    [HtmlTargetElement(Attributes = _durationStartAttributeName)]
+    public class ElapsedDurationTagHelper : TagHelper
+    {
+        private const string _durationStartAttributeName = "duration-start";
+
+        /// <summary>
+        /// Start of elapsed duration.
+        /// </summary>
+        [HtmlAttributeName(_durationStartAttributeName)]
+        public DateTime DurationStart { get; set; }
+
+        /// <summary>
+        /// Modifies contents of span elements with the duration-start attribute.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="output"></param>
+        /// <returns></returns>
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetContent((DateTime.UtcNow - DurationStart).ToElapsedDurationString());
+        }
+    }
+}
