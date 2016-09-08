23890155aaac6ba5b8387f79187aefa66ffbe999
JeremyTan <jeremytanchongdao@gmail.com>
2016-09-08 15:32:40 +0800

    Added CheckEmailAndPasswordExists and UpdateAccountAlternativeEmail sprocs
    - Project xml file auto updated too.

---
+++ Jering.AccountManagement.Database/Website/Stored Procedures/Accounts/CheckEmailAndPasswordExists.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [Website].[CheckEmailAndPasswordExist]
+	@Email NVARCHAR(256),
+	@Password NVARCHAR(256)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	SELECT
+	   CASE WHEN EXISTS(SELECT * FROM [Accounts] WHERE [Email] = @Email AND [PasswordHash] = HASHBYTES(N'SHA2_256', @Password + CONVERT(char(36), [Salt])))
+	   THEN 1 
+	   ELSE 0 
+	   END 
+END
+
---
+++ Jering.AccountManagement.Database/Website/Stored Procedures/Accounts/UpdateAccountAlternativeEmail.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [Website].[UpdateAccountAlternativeEmail]
+	@AccountId INT,
+	@AlternativeEmail NVARCHAR(256)
+AS
+BEGIN	
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	UPDATE [dbo].[Accounts]
+	SET [SecurityStamp] = NEWID(), 
+		[AlternativeEmail] = @AlternativeEmail
+	WHERE AccountId = @AccountId;
+
+	SELECT @@ROWCOUNT;
+END
