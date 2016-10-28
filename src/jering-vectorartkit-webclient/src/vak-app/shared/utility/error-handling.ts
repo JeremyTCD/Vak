9925c6744f3516841fd687ba7f5d6821e7a3f442
JeremyTan <jeremytanchongdao@gmail.com>
2016-10-28 15:35:56 +0800

    Added handleUnexpectedError shared function to web client

---
+++ src/jering-vectorartkit-webclient/src/vak-app/shared/utility/error-handling.ts
@@ -0,0 +1,26 @@
+﻿import { Response } from '@angular/http';
+import { Router } from '@angular/router';
+import { Check } from './check';
+import { environment } from '../../../environments/environment';
+
+export function handleUnexpectedError(router: Router, error: any) {
+    if (Check.isString(error)) {
+        router.navigate([`/error`, { errorMessage: error }]);
+        return;
+    }
+    else if (error instanceof Response) {
+        router.navigate([`/error`, { errorMessage: error.json().errorMessage }]);
+        return;
+    }
+    else if (error instanceof Error && !environment.production) {
+        router.navigate([`/error`,
+            {
+                errorMessage: `name: ${error.name}\n
+                               message: ${error.message}\n
+                               stack: ${error.stack}\n`
+            }
+        ]);
+    }
+
+    router.navigate([`/error`]);
+}
