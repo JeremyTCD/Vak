fedbc987dc77a198a75bca6c8d76db14a120a9e7
JeremyTan <jeremytanchongdao@gmail.com>
2016-10-28 16:06:56 +0800

    Added DynamicControlAsyncValidator class.

---
+++ src/jering-vectorartkit-webclient/src/vak-app/shared/dynamic-forms/dynamic-control/dynamic-control-async-validator.ts
@@ -0,0 +1,14 @@
+﻿import { DynamicControlValidator } from './dynamic-control-validator';
+import { Subscription } from 'rxjs';
+
+/**
+ * Data used to defined a DynamicControlValidator
+ */
+export class DynamicControlAsyncValidator {
+    constructor (public validate: DynamicControlValidator, private _subscription: Subscription){
+    }
+
+    unsubscribe(): void {
+        this._subscription.unsubscribe();
+    }
+}
