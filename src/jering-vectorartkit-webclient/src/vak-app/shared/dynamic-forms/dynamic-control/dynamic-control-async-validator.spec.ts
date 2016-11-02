d5bcc91fef3ab0bca959b116244a9a77609875ad
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-02 20:51:28 +0800

    Removed DynamicControlValidators.validateAsync. Moved logic to DynamicControlAsyncValidator.
    - Moved and cleaned up tests as well.
    - This change makes it easier to test the AsyncValidator since rxjs objects such as the subject, observable and subscription are exposed.

---
+++ src/jering-vectorartkit-webclient/src/vak-app/shared/dynamic-forms/dynamic-control/dynamic-control-async-validator.spec.ts
@@ -0,0 +1,92 @@
+﻿import { DynamicControlAsyncValidator } from './dynamic-control-async-validator';
+import { DynamicControl } from './dynamic-control';
+import { Validity } from '../validity';
+import { DynamicControlValidatorResult } from './dynamic-control-validator-result';
+
+let testErrorMessage = `testErrorMessage`;
+let testDisplayName = `testDisplayName`;
+
+describe(`DynamicControlAsyncValidator`, () => {
+    let validateAsyncValidator: DynamicControlAsyncValidator;
+
+    it(`Constructor creates an open subscription`, () => {
+        validateAsyncValidator = new DynamicControlAsyncValidator({
+            name: `validateAsync`,
+            errorMessage: testErrorMessage,
+            options: null
+        },
+            null,
+            null);
+
+        expect(validateAsyncValidator.subscription.closed).toBe(false);
+    });
+
+    describe(`validate`, () => {
+        let dynamicControl: DynamicControl<any>;
+
+        beforeEach(() => {
+            dynamicControl = new DynamicControl({ displayName: testDisplayName });
+            validateAsyncValidator = new DynamicControlAsyncValidator({
+                name: `validateAsync`,
+                errorMessage: `{0} testErrorMessage`,
+                options: null
+            },
+                dynamicControl,
+                null);
+        });
+
+        it(`Returns DynamicControlValidatorResult with validity = Validity.invalid and message = undefined if 
+            dynamicControl is invalid before function is called. Unsubscribes from subscription.`, () => {
+                dynamicControl.validity = Validity.invalid;
+
+                let result = validateAsyncValidator.validate(dynamicControl);
+
+                expect(validateAsyncValidator.subscription.closed).toBe(true);
+                expect(result).toBeDefined();
+                expect(result.validity).toBe(Validity.invalid);
+                expect(result.message).toBe(undefined);
+            });
+
+        it(`Returns DynamicControlValidatorResult with validity = Validity.valid and message = undefined if
+            control value is null, undefined or an empty string. Unsubscribes from subscription.`, () => {
+                let result: DynamicControlValidatorResult;
+
+                dynamicControl.validity = Validity.valid;
+
+                dynamicControl.value = null;
+                result = validateAsyncValidator.validate(dynamicControl);
+                expect(validateAsyncValidator.subscription.closed).toBe(true);
+                expect(result).toBeDefined();
+                expect(result.validity).toBe(Validity.valid);
+                expect(result.message).toBe(undefined);
+
+                dynamicControl.value = undefined;
+                result = validateAsyncValidator.validate(dynamicControl);
+                expect(validateAsyncValidator.subscription.closed).toBe(true);
+                expect(result).toBeDefined();
+                expect(result.validity).toBe(Validity.valid);
+                expect(result.message).toBe(undefined);
+
+                dynamicControl.value = ``;
+                result = validateAsyncValidator.validate(dynamicControl);
+                expect(validateAsyncValidator.subscription.closed).toBe(true);
+                expect(result).toBeDefined();
+                expect(result.validity).toBe(Validity.valid);
+                expect(result.message).toBe(undefined);
+            });
+
+        it(`Returns DynamicControlValidatorResult with validity = Validity.pending and message = undefined if 
+            dynamicControl is valid before function is called. Ensures that subscription is open.`, () => {
+                validateAsyncValidator.subscription.unsubscribe();
+                dynamicControl.validity = Validity.valid;
+                dynamicControl.value = `test`;
+
+                let result = validateAsyncValidator.validate(dynamicControl);
+
+                expect(validateAsyncValidator.subscription.closed).toBe(false);
+                expect(result).toBeDefined();
+                expect(result.validity).toBe(Validity.pending);
+                expect(result.message).toBe(undefined);
+            });
+    });
+});
--- src/jering-vectorartkit-webclient/src/vak-app/shared/dynamic-forms/dynamic-control/dynamic-control-async-validator.ts
+++ src/jering-vectorartkit-webclient/src/vak-app/shared/dynamic-forms/dynamic-control/dynamic-control-async-validator.ts
@@ -1,15 +1,94 @@
-﻿import { DynamicControlValidator } from './dynamic-control-validator';
-import { Subscription } from 'rxjs';
+﻿import { Subscription, Subject, Observable, Observer } from 'rxjs';
 
-/**
- * Data used to defined a DynamicControlValidator
- */
+import { DynamicFormsService } from '../dynamic-forms.service';
+import { DynamicControlValidator } from './dynamic-control-validator';
+import { DynamicControlValidatorData } from './dynamic-control-validator-data';
+import { DynamicControl } from './dynamic-control';
+import { Validity } from '../validity';
+import { environment } from '../../../../environments/environment';
+import { DynamicControlValidatorResult } from './dynamic-control-validator-result';
+import { Check } from '../../utility/check';
+
 export class DynamicControlAsyncValidator {
-    constructor(public validate: DynamicControlValidator, private _subscription: Subscription) {
+    subject = new Subject<string>();
+    subjectAsObservable: Observable<Validity>;
+    observer: Observer<Validity>;
+    subscription: Subscription;
+
+    /**
+     * Constructor
+     *
+     * Instantiates subjectAsObservable, observer and subscription.
+     *
+     * observer
+     * - Directly sets dynamicControl.validity to Validity.valid if query observable returns Validity.valid
+     * - Otherwise, directly sets dynamicControl.validity to Validity.invalid
+     */
+    constructor(validatorData: DynamicControlValidatorData, dynamicControl: DynamicControl<any>,
+        dynamicFormsService: DynamicFormsService) {
+
+        this.subjectAsObservable = this.subject.
+            debounceTime(200).
+            map((value: string) => {
+                return dynamicFormsService.
+                    validateValue(`${environment.apiUrl}${validatorData.options[`RelativeUrl`]}`, value).
+                    catch((error: any) =>
+                        Observable.of(Validity.valid)
+                    );
+            }).
+            switch();
+
+        this.observer = {
+            next: validity => {
+                if (validity === Validity.valid) {
+                    dynamicControl.validity = Validity.valid;
+                } else {
+                    dynamicControl.validity = Validity.invalid;
+                    dynamicControl.messages.push(validatorData.errorMessage);
+                }
+                dynamicControl.tryValidateParent();
+            },
+            error: error => {
+                dynamicControl.validity = Validity.valid;
+            },
+            complete: () => {
+                // Do nothing
+            }
+        }
+
+        this.subscription = this.subjectAsObservable.subscribe(this.observer);
     }
 
-    unsubscribe(): void {
-        this._subscription.unsubscribe();
+    /**
+     * Unsubscribes from subscription is invalid before function is called or if dynamicControl.value
+     * is not a value. Otherwise, subscribes to subjectAsObservable if subscription is closed and calls
+     * subject.next.
+     * 
+     * Returns
+     * - DynamicControlValidatorResult with validity = Validity.pending and message = undefined if
+     *   dynamicControl.validity is valid before function is called
+     * - DynamicControlValidatorResult with validity = Validity.invalid and message = undefined if
+     *   dynamicControl.validity is invalid before function is called
+     * - DynamicControlValidatorResult with validity = Validity.valid and message = undefined if
+     *   control value is null, undefined or an empty string
+     */
+    validate(dynamicControl: DynamicControl<any>): DynamicControlValidatorResult {
+        if (dynamicControl.validity === Validity.invalid) {
+            this.subscription.unsubscribe();
+
+            return new DynamicControlValidatorResult(Validity.invalid);
+        } else if (!Check.isValue(dynamicControl.value)) {
+            this.subscription.unsubscribe();
+
+            return new DynamicControlValidatorResult(Validity.valid);
+        } else {
+            if (this.subscription.closed) {
+                this.subscription = this.subjectAsObservable.subscribe(this.observer);
+            }
+            this.subject.next(dynamicControl.value);
+
+            return new DynamicControlValidatorResult(Validity.pending);
+        }
     }
 }
 
