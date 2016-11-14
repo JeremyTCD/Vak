f0748dc00355b940c8a6b2320abdf28a78ea8a70
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-14 18:54:51 +0800

    Added StorageService. Wraps localStorage and sessionStorage to facilitate testing and browser support.

---
+++ src/jering-vectorartkit-webclient/src/vak-app/shared/storage.service.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+
+/**
+ * Provides storage interface
+ */
+@Injectable()
+export class StorageService {
+    setItem(key: string, data: string, isPersistent: boolean): void {
+        if (isPersistent) {
+            localStorage.setItem(key, data);
+        } else {
+            sessionStorage.setItem(key, data);
+        }
+    }
+
+    getItem(key: string): string {
+        return localStorage.getItem(key) || sessionStorage.getItem(key);
+    }
+
+    removeItem(key: string): void {
+        localStorage.removeItem(key);
+        sessionStorage.removeItem(key);
+    }
+}
