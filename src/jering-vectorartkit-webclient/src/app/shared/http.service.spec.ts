e753e5f5e709836d3706380751e06c16d351e171
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-17 20:53:56 +0800

    Added HttpService

---
+++ src/jering-vectorartkit-webclient/src/app/shared/http.service.spec.ts
@@ -0,0 +1,188 @@
+﻿import { Http, RequestOptionsArgs, Response, ResponseOptions, RequestMethod } from '@angular/http';
+import { TestBed, inject } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+
+import { HttpService } from './http.service';
+import { ErrorHandlerService } from './utility/error-handler.service';
+import { environment } from '../../environments/environment';
+import { ErrorResponseModel } from './response-models/error.response-model';
+
+let testRelativeUrl = `testRelativeUrl`;
+let testUrl = `${environment.apiDomain}${testRelativeUrl}`;
+let testRequestBody = {};
+let stubResponseModel: StubResponseModel = { testProperty: `testProperty` };
+
+describe(`HttpService`, () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [HttpService,
+                { provide: ErrorHandlerService, useClass: StubErrorHandlerService },
+                { provide: Http, useClass: StubHttp }]
+        });
+    });
+
+    it(`post creates and prepares RequestOptions for a post request and calls request with prepared options`,
+        inject([HttpService], (httpService: HttpService) => {
+            let spy = spyOn(httpService, `request`);
+
+            httpService.post(testRelativeUrl, testRequestBody);
+
+            let args = spy.calls.first().args;
+            expect(args[0]).toBe(testRelativeUrl);
+            let requestOptionsArgs = args[1] as RequestOptionsArgs;
+            expect(requestOptionsArgs.method).toBe(RequestMethod.Post);
+            expect(requestOptionsArgs.body).toBe(testRequestBody);
+        })
+    );
+
+    it(`get creates and prepares RequestOptions for a get request and calls request with prepared options`,
+        inject([HttpService], (httpService: HttpService) => {
+            let spy = spyOn(httpService, `request`);
+
+            httpService.get(testRelativeUrl);
+
+            let args = spy.calls.first().args;
+            expect(args[0]).toBe(testRelativeUrl);
+            let requestOptionsArgs = args[1] as RequestOptionsArgs;
+            expect(requestOptionsArgs.method).toBe(RequestMethod.Get);
+        })
+    );
+
+    describe(`request`, () => {
+        let testResponse: Response;
+
+        it(`Prepares RequestOptions for a request and calls Http.request`,
+            inject([HttpService, Http], (httpService: HttpService, http: StubHttp) => {
+                testResponse = new Response(new ResponseOptions({}));
+                let spy = spyOn(http, `request`).and.returnValue(Observable.of(testResponse));
+
+                httpService.request(testRelativeUrl, {});
+
+                let args = spy.calls.first().args;
+                expect(args[0]).toBe(testUrl);
+                let requestOptionsArgs = args[1] as RequestOptionsArgs;
+                expect(requestOptionsArgs.withCredentials).toBe(true);
+                expect(requestOptionsArgs.headers.get(`X-Requested-With`)).toBe(`XMLHttpRequest`);
+            })
+        );
+
+        it(`Returns deserialized body of Response if request succeeds`,
+            inject([HttpService, Http], (httpService: HttpService, http: StubHttp) => {
+                testResponse = new Response(new ResponseOptions({ body: JSON.stringify(stubResponseModel) }));
+                spyOn(http, `request`).and.returnValue(Observable.of(testResponse));
+                let result: StubResponseModel;
+
+                httpService.
+                    request(testRelativeUrl, {}).
+                    subscribe(responseModel => result = responseModel);
+
+                expect(result).toEqual(stubResponseModel);
+            })
+        );
+
+        describe(`If request fails`, () => {
+            let errorResponseModel: ErrorResponseModel;
+            let nextOrErrorCalled: boolean;
+            let completeCalled: boolean;
+            let testObserver = {
+                    next: next => nextOrErrorCalled = true,
+                    error: error => nextOrErrorCalled,
+                    complete: () => completeCalled = true
+            };
+
+            beforeEach(() => {
+                nextOrErrorCalled = false;
+                completeCalled = false;
+            });
+
+            it(`Returns deserialized body of Response if error is expected`,
+                inject([HttpService, Http], (httpService: HttpService, http: StubHttp) => {
+                    errorResponseModel = { expectedError: true };
+                    testResponse = new Response(new ResponseOptions({ body: JSON.stringify(errorResponseModel) }));
+                    spyOn(http, `request`).and.returnValue(Observable.of(testResponse));
+                    let result: StubResponseModel;
+
+                    httpService.
+                        request(testRelativeUrl, {}).
+                        subscribe(responseModel => result = responseModel);
+
+                    expect(result).toEqual(errorResponseModel);
+                })
+            );
+
+            it(`Calls ErrorHandlerService.handleUnexpectedError if error is unexpected. Calls complete
+                observer immediately, does not call next or error observer.`,
+                inject([HttpService, Http, ErrorHandlerService], (httpService: HttpService, http: StubHttp, errorHandlerService: StubErrorHandlerService) => {
+                    errorResponseModel = { expectedError: false };
+                    testResponse = new Response(new ResponseOptions({ body: JSON.stringify(errorResponseModel) }));
+                    spyOn(errorHandlerService, `handleUnexpectedError`);
+                    spyOn(http, `request`).and.returnValue(Observable.throw(testResponse));
+
+                    httpService.
+                        request(testRelativeUrl, {}).
+                        subscribe(testObserver);
+
+                    expect(nextOrErrorCalled).toBe(false);
+                    expect(completeCalled).toBe(true);
+                    expect(errorHandlerService.handleUnexpectedError).toHaveBeenCalledWith(testResponse);
+                })
+            );
+
+            it(`Calls ErrorHandlerService.handleUnexpectedError if error is not a response. Calls complete
+                observer immediately, does not call next or error observer.`,
+                inject([HttpService, Http, ErrorHandlerService], (httpService: HttpService, http: StubHttp, errorHandlerService: StubErrorHandlerService) => {
+                    let testErrorObject = {};
+                    spyOn(errorHandlerService, `handleUnexpectedError`);
+                    spyOn(http, `request`).and.returnValue(Observable.throw(testErrorObject));
+
+                    httpService.
+                        request(testRelativeUrl, {}).
+                        subscribe(testObserver);
+
+                    expect(nextOrErrorCalled).toBe(false);
+                    expect(completeCalled).toBe(true);
+                    expect(errorHandlerService.handleUnexpectedError).toHaveBeenCalledWith(testErrorObject );
+                })
+            );
+
+            it(`Calls ErrorHandlerService.handleUnexpectedError if error does not have a body in json format`,
+                inject([HttpService, Http, ErrorHandlerService], (httpService: HttpService, http: StubHttp, errorHandlerService: StubErrorHandlerService) => {
+                    testResponse = new Response(new ResponseOptions({ body: null }));
+                    spyOn(http, `request`).and.returnValue(Observable.throw(testResponse));
+                    spyOn(errorHandlerService, `handleUnexpectedError`);
+
+                    httpService.
+                        request(testRelativeUrl, {}).
+                        subscribe(testObserver);
+
+                    expect(nextOrErrorCalled).toBe(false);
+                    expect(completeCalled).toBe(true);
+                    expect(errorHandlerService.handleUnexpectedError).toHaveBeenCalledWith(testResponse);
+                })
+            );
+        });
+    });
+});
+
+interface StubResponseModel {
+    testProperty: string;
+}
+
+class StubErrorHandlerService {
+    handleUnexpectedError(error: any): void {
+    }
+}
+
+class StubHttp {
+    get(url: string, options?: RequestOptionsArgs, domain?: string): Observable<Response> {
+        return new Observable<Response>();
+    };
+
+    post(url: string, body: string, options?: RequestOptionsArgs, domain?: string): Observable<Response> {
+        return new Observable<Response>();
+    };
+
+    request(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        return new Observable<Response>();
+    }
+}
---
+++ src/jering-vectorartkit-webclient/src/app/shared/http.service.ts
@@ -0,0 +1,96 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, RequestOptionsArgs, Response, RequestMethod } from '@angular/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { environment } from '../../environments/environment';
+import { ErrorHandlerService } from './utility/error-handler.service';
+import { ErrorResponseModel } from './response-models/error.response-model';
+
+/**
+ * Provides Http helpers
+ */
+@Injectable()
+export class HttpService {
+    constructor(private _http: Http, private _errorHandlerService: ErrorHandlerService) { }
+
+    /**
+     * Sends post request
+     *
+     * Returns
+     * - Observable<any>
+     */
+    post(relativeUrl: string, body: any, options?: RequestOptionsArgs, domain?: string): Observable<any> {
+        options = options || new RequestOptions();
+        // Note that Http.request assigns content-type based on body's type
+        options.body = body;
+        options.method = RequestMethod.Post;
+
+        return this.request(relativeUrl, options, domain);
+    }
+
+    /**
+     * Sends get request
+     *
+     * Returns
+     * - Observable<any>
+     */
+    get(relativeUrl: string, options?: RequestOptionsArgs, domain?: string): Observable<any> {
+        options = options || new RequestOptions();
+        options.method = RequestMethod.Get;
+
+        return this.request(relativeUrl, options, domain);
+    }
+
+    /**
+     * Sends request of any method
+     *
+     * Returns
+     * - Observable<any>
+     */
+    request(relativeUrl: string, options: RequestOptionsArgs, domain?: string): Observable<any> {
+        if (!options.headers) {
+            options.headers = new Headers();
+        }
+
+        // Required to prevent AspNetCore redirecting on failed authentication
+        options.headers.append(`X-Requested-With`, `XMLHttpRequest`);
+        // Required by AspNetCore authentication (sends cookies with XMLHttpRequest)
+        options.withCredentials = true;
+
+        return this.
+            _http.
+            request(this.urlFromRelativeUrl(relativeUrl, domain), options).
+            map(response => response.json()).
+            catch(error => this.handleRequestError(error));
+    }
+
+    /**
+     * Handles a request error. Passes error on to error Observer if error is expected. Otherwise,
+     * passes error to ErrorHandlerService.
+     *
+     * Returns
+     * - ErrorObservable if error is a Response with ErrorResponseModel as its body and
+     *   body.expectedError is true.
+     * - Otherwise, empty Observable.   
+     */
+    private handleRequestError(error: any): Observable<any> {
+        let body: ErrorResponseModel;
+
+        if (error instanceof Response &&
+            (body = error.json()) &&
+            body.expectedError) {
+
+            // Pass expected errors on to error observer
+            return Observable.throw(body);
+        }
+
+        // Pass unexpected errors on to ErrorHandlerService
+        this._errorHandlerService.handleUnexpectedError(error);
+        return Observable.empty();
+    }
+
+    private urlFromRelativeUrl(relativeUrl: string, domain?: string): string {
+        return `${domain ? domain : environment.apiDomain}${relativeUrl}`;
+    }
+}
