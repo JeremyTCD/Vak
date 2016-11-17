b01981aaa21268c62314db0af8cc44fd8d7d9e08
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-17 21:39:18 +0800

    DynamicFormService now uses HttpService instead of Http

---
+++ src/jering-vectorartkit-webclient/src/app/shared/dynamic-forms/dynamic-forms.service.spec.ts
@@ -0,0 +1,148 @@
+﻿import { TestBed, inject } from '@angular/core/testing';
+import { Response, ResponseOptions, RequestOptionsArgs, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { DynamicControl } from './dynamic-control/dynamic-control';
+import { DynamicForm } from './dynamic-form/dynamic-form';
+import { DynamicFormResponseModel } from './response-models/dynamic-form.response-model';
+import { DynamicFormsService } from './dynamic-forms.service';
+import { ValidateResponseModel } from '../response-models/validate.response-model';
+import { ErrorHandlerService } from '../utility/error-handler.service';
+import { environment } from '../../../environments/environment';
+import { Validity } from './validity';
+import { HttpService } from '../http.service';
+
+let testFormModelName = `testFormModelName`;
+let testControlName = `testControlName`;
+let testMessage = `testMessage`;
+let testRelativeUrl = `testRelativeUrl`;
+let testDynamicControl: DynamicControl<any>;
+let testDynamicForm: DynamicForm;
+let testResponse: Response;
+let testDynamicFormResponseModel: DynamicFormResponseModel;
+
+describe('DynamicFormsService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [DynamicFormsService, { provide: ErrorHandlerService, useClass: StubErrorHandlerService }, { provide: HttpService, useClass: StubHttpService }]
+        });
+        testDynamicControl = new DynamicControl<any>({ name: testControlName });
+        testDynamicForm = new DynamicForm([testDynamicControl], testMessage);
+        testResponse = new Response(
+            new ResponseOptions({})
+        );
+    });
+
+    describe(`getDynamicForm`, () => {
+        beforeEach(() => {
+            testDynamicFormResponseModel = { dynamicControlResponseModels: [], errorMessage: testMessage};
+        });
+
+        it('Calls Http.get',
+            inject([DynamicFormsService, HttpService], (dynamicFormsService: DynamicFormsService, stubHttpService: StubHttpService) => {
+                let getSpy = spyOn(stubHttpService, `get`).and.returnValue(Observable.of(testDynamicFormResponseModel));
+
+                dynamicFormsService.
+                    getDynamicForm(testFormModelName).
+                    subscribe(dynamicForm => null);
+
+                expect(stubHttpService.get).toHaveBeenCalledTimes(1);
+                let args = getSpy.calls.first().args;
+                expect(args[0]).toBe(`DynamicForms/GetDynamicForm`);
+                let requestOptions = args[1] as RequestOptionsArgs;
+                let urlSearchParams = requestOptions.search as URLSearchParams;
+                expect(urlSearchParams.get(`formModelName`)).toBe(testFormModelName);
+            })
+        );
+
+        it(`Maps response to a DynamicFormResponseModel if Http.get succeeds`,
+            inject([DynamicFormsService, HttpService], (dynamicFormsService: DynamicFormsService, stubHttpService: StubHttpService) => {
+                spyOn(stubHttpService, `get`).and.returnValue(Observable.of(testDynamicFormResponseModel));
+                let result: DynamicForm;
+
+                dynamicFormsService.
+                    getDynamicForm(testFormModelName).
+                    subscribe(dynamicForm => result = dynamicForm);
+
+                expect(result instanceof DynamicForm).toBe(true);
+                expect(result.dynamicControls.length).toBe(0);
+                expect(result.message).toBe(testMessage);
+            })
+        );
+    });
+
+    it(`submitDynamicForm calls Http.post`, inject([DynamicFormsService, HttpService], (dynamicFormsService: DynamicFormsService, stubHttpService: StubHttpService) => {
+        let getSpy = spyOn(stubHttpService, `post`).and.returnValue(Observable.of(testResponse));
+
+        dynamicFormsService.
+            submitDynamicForm(testRelativeUrl, testDynamicForm).
+            subscribe(response => null);
+
+        expect(stubHttpService.post).toHaveBeenCalledTimes(1);
+        let args = getSpy.calls.first().args;
+        expect(args[0]).toBe(testRelativeUrl);
+        expect(args[1]).toEqual(testDynamicForm.value);
+    })
+    );     
+
+    describe(`validateValue`, () => {
+        let testValue = `testValue`;
+
+        beforeEach(() => {
+            testResponse = new Response(
+                new ResponseOptions({})
+            );
+        });
+
+        it('Calls Http.get',
+            inject([DynamicFormsService, HttpService], (dynamicFormsService: DynamicFormsService, stubHttpService: StubHttpService) => {
+                let getSpy = spyOn(stubHttpService, `get`).and.returnValue(Observable.of(testResponse));
+
+                dynamicFormsService.
+                    validateValue(testRelativeUrl, testValue).
+                    subscribe(validity => null);
+
+                expect(stubHttpService.get).toHaveBeenCalledTimes(1);
+                let args = getSpy.calls.first().args;
+                expect(args[0]).toBe(testRelativeUrl);
+                let requestOptions = args[1] as RequestOptionsArgs;
+                let urlSearchParams = requestOptions.search as URLSearchParams;
+                expect(urlSearchParams.get(`value`)).toBe(testValue);
+            })
+        );
+
+        it(`Maps ValidateResponseModel to a Validity value if Http.get succeeds`,
+            inject([DynamicFormsService, HttpService], (dynamicFormsService: DynamicFormsService, stubHttpService: StubHttpService) => {
+                let testValidateResponseModel: ValidateResponseModel = {valid: true};
+                testResponse = new Response(
+                    new ResponseOptions({
+                        body: JSON.stringify(testValidateResponseModel)
+                    })
+                );
+                spyOn(stubHttpService, `get`).and.returnValue(Observable.of(testResponse));
+                let result: Validity;
+
+                dynamicFormsService.
+                    validateValue(testRelativeUrl, testValue).
+                    subscribe(validity => result = validity);
+
+                expect(result).toBe(Validity.valid);
+            })
+        );
+    });
+});
+
+class StubErrorHandlerService {
+    handleUnexpectedError(error: any): void {
+    }
+}
+
+class StubHttpService {
+    get(url: string, options?: RequestOptionsArgs, domain?: string): Observable<any> {
+        return Observable.of(testResponse);
+    };
+
+    post(url: string, body: string, options?: RequestOptionsArgs, domain?: string): Observable<any> {
+        return Observable.of(testResponse);
+    };
+}
---
+++ src/jering-vectorartkit-webclient/src/app/shared/dynamic-forms/dynamic-forms.service.ts
@@ -0,0 +1,107 @@
+﻿import { Injectable } from '@angular/core';
+import { Response, URLSearchParams, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { environment } from '../../../environments/environment';
+import { HttpService } from '../http.service';
+import { DynamicForm } from './dynamic-form/dynamic-form';
+import { DynamicFormResponseModel } from './response-models/dynamic-form.response-model';
+import { ErrorResponseModel } from '../response-models/error.response-model';
+import { ValidateResponseModel } from '../response-models/validate.response-model';
+import { DynamicControl } from './dynamic-control/dynamic-control';
+import { Validity } from './validity';
+
+/**
+ * Wraps Http requests and response processing for the dynamic forms system.
+ */
+@Injectable()
+export class DynamicFormsService {
+    private _dynamicFormsRelativeUrl = `DynamicForms/GetDynamicForm`;
+
+    constructor(private _httpService: HttpService) { }
+
+    /**
+     * Sends get request to retrieve data for a DynamicForm.
+     * Registers a function that maps retrieved data to a DynamicForm.
+     *
+     * Returns
+     * - Observable<DynamicForm> if get request succeeds.
+     */
+    getDynamicForm(formModelName: string): Observable<DynamicForm> {
+        let urlSearchParams = new URLSearchParams();
+        urlSearchParams.set(`formModelName`, formModelName);
+        let requestOptions = new RequestOptions({withCredentials: true, search: urlSearchParams });
+
+        return this._httpService.
+            get(this._dynamicFormsRelativeUrl, requestOptions).
+            map(this.dynamicFormFromResponseModel, this);
+    }
+
+    /**
+     * Converts a DynamicFormResponseModel into a DynamicForm
+     *
+     * Returns
+     * - DynamicForm
+     */
+    private dynamicFormFromResponseModel(formResponseModel: DynamicFormResponseModel): DynamicForm {
+        let dynamicControls: DynamicControl<any>[] = [];
+
+        for (let controlResponseModel of formResponseModel.dynamicControlResponseModels) {
+            dynamicControls.push(new DynamicControl<any>(controlResponseModel, this));
+        }
+
+        let dynamicForm = new DynamicForm(dynamicControls.sort((dynamicControl1, dynamicControl2) => dynamicControl1.order - dynamicControl2.order), formResponseModel.errorMessage);
+        dynamicForm.setupContext();
+
+        return dynamicForm;
+    }
+
+    /**
+     * Sends post request to submit a DynamicForm's value.
+     * Registers catch function handleSubmitDynamicFormError.
+     *
+     * Returns
+     * - Observable<ResponseModel> if post request succeeds.
+     * - Observable<ErrorResponseModel> containing validation errors (model state) if form validation fails.
+     */
+    submitDynamicForm(relativeUrl: string, dynamicForm: DynamicForm): Observable<any> {
+        return this.
+            _httpService.
+            post(relativeUrl, dynamicForm.value);
+    }
+
+    /**
+     * Sends get request to validate a value.
+     * Registers a function that maps response to a boolean.
+     *
+     * Returns
+     * - Observable<Validity> with Validity.valid if value is valid
+     * - Otherwise, Observable<Validity> with Validity.invalid 
+     */
+    validateValue(relativeUrl: string, value: string): Observable<Validity> {
+        let urlSearchParams = new URLSearchParams();
+        urlSearchParams.set(`value`, value);
+        let requestOptions = new RequestOptions({ search: urlSearchParams });
+
+        return this._httpService.
+            get(relativeUrl, requestOptions).
+            map(this.validityFromResponseModel);
+    }
+
+    /**
+     * Converts a ValidityResponseModel into a Validity value.
+     *
+     * Returns
+     * - Validity.invalid if ValidateResponseModel.valid is false
+     * - Validity.valid otherwise
+     */
+    private validityFromResponseModel(responseModel: ValidateResponseModel): Validity{
+        if (responseModel.valid === false) {
+            return Validity.invalid;
+        }
+
+        return Validity.valid;
+    }
+}
