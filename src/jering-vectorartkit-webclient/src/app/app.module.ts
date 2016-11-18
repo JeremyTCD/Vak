f61dff8e3e3663c0916c583deccb07ef8a796f2e
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-18 15:01:47 +0800

    AppModule now has ErrorHandlerService as a provider. Now also imports HttpModule and LogInModule.

--- src/jering-vectorartkit-webclient/src/app/app.module.ts
+++ src/jering-vectorartkit-webclient/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
 
 import { SignUpModule } from './sign-up/sign-up.module';
+import { LogInModule } from './log-in/log-in.module';
 import { HomeModule } from './home/home.module';
 import { ErrorModule } from './error/error.module';
 import { UserService } from './shared/user.service';
 import { StorageService } from './shared/storage.service';
 import { HttpService } from './shared/http.service';
+import { ErrorHandlerService } from './shared/error-handler.service';
 
 @NgModule({
     imports: [
+        HttpModule,
         BrowserModule,
         CommonModule,
         SignUpModule,
+        LogInModule,
         HomeModule,
         ErrorModule,
         AppRouting
@@ -24,7 +29,8 @@ import { HttpService } from './shared/http.service';
     declarations: [AppComponent],
     providers: [UserService,
         StorageService,
-        HttpService],
+        HttpService,
+        ErrorHandlerService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
