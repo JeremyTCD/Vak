e154018ba6181942d5fdd20b43e4e15e2bead35c
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-18 21:12:53 +0800

    Added buttonText field to DynamicFormResponseModel

--- src/jering-vectorartkit-webclient/src/app/shared/dynamic-forms/response-models/dynamic-form.response-model.ts
+++ src/jering-vectorartkit-webclient/src/app/shared/dynamic-forms/response-models/dynamic-form.response-model.ts
@@ -2,5 +2,6 @@ import { DynamicControlResponseModel } from './dynamic-control.response-model';
 
 export interface DynamicFormResponseModel {
 	errorMessage?: string;
+	buttonText?: string;
 	dynamicControlResponseModels?: DynamicControlResponseModel[];
 }
