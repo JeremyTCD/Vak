a493608099d3dc79824dc932a5093de6414be290
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-18 15:03:00 +0800

    Removed ErrorHandlerService and HttpModule from SignUpModule. ErrorHandlerService is now provided by AppModule and HttpModule is only used by services provided by AppModule.

--- src/jering-vectorartkit-webclient/src/app/sign-up/sign-up.module.ts
+++ src/jering-vectorartkit-webclient/src/app/sign-up/sign-up.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 
 import { SignUpComponent } from './sign-up.component';
 import { SignUpRouting } from './sign-up.routing';
 import { DynamicFormsModule } from '../shared/dynamic-forms/dynamic-forms.module';
-import { ErrorHandlerService } from '../shared/utility/error-handler.service';
 import { DynamicFormsResolve } from '../shared/dynamic-forms/dynamic-forms-resolve';
 
 @NgModule({
     imports: [
-        HttpModule,
         SignUpRouting,
         DynamicFormsModule
     ],
     providers: [
-        ErrorHandlerService,
         DynamicFormsResolve
     ],
     declarations: [
