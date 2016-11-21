ef191a2152c528c38dc51442a761cc7f7bbdd138
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-21 12:11:25 +0800

    Added TwoFactorLogIn form and response models

--- src/Jering.VectorArtKit.WebApi/FormModels/Account/TwoFactorLogInFormModel.cs
+++ src/Jering.VectorArtKit.WebApi/FormModels/Account/TwoFactorLogInFormModel.cs
@@ -1,4 +1,5 @@
 ﻿using Jering.DataAnnotations;
+using Jering.DynamicForms;
 using Jering.VectorArtKit.WebApi.Resources;
 using System;
 using System.Collections.Generic;
@@ -8,10 +9,13 @@ using System.Threading.Tasks;
 
 namespace Jering.VectorArtKit.WebApi.FormModels
 {
-    public class VerifyTwoFactorCodeFormModel
+    [DynamicForm(nameof(Strings.ErrorMessage_Form_Invalid), nameof(Strings.ButtonText_Submit), typeof(Strings))]
+    public class TwoFactorLogInFormModel
     {
         [ValidateLength(6, nameof(Strings.ErrorMessage_TwoFactorCode_Invalid), typeof(Strings))]
-        [Required]
+        [ValidateRequired(nameof(Strings.ErrorMessage_TwoFactorCode_Required), typeof(Strings))]
+        [DynamicControl("input", nameof(Strings.DisplayName_TwoFactorCode), typeof(Strings), 1)]
+        [DynamicControlProperty("type", "text")]
         public string Code { get; set; }
 
         public bool IsPersistent { get; set; }
---
+++ src/Jering.VectorArtKit.WebApi/ResponseModels/Account/TwoFactorLogInResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jering.VectorArtKit.WebApi.ResponseModels.Account
+{
+    public class TwoFactorLogInResponseModel
+    {
+        public string Username { get; set; }
+        public bool IsPersistent { get; set; }
+    }
+}
