085617378a0f8458ac6682b4eea46822fa137438
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-21 18:41:15 +0800

    Added TwoFactorLogInComponent declaration to LogInModule

--- src/jering-vectorartkit-webclient/src/app/log-in/log-in.module.ts
+++ src/jering-vectorartkit-webclient/src/app/log-in/log-in.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { LogInComponent } from './log-in.component';
+import { TwoFactorLogInComponent } from './two-factor-log-in/two-factor-log-in.component';
 import { LogInRouting } from './log-in.routing';
 import { DynamicFormsModule } from '../shared/dynamic-forms/dynamic-forms.module';
 import { DynamicFormsResolve } from '../shared/dynamic-forms/dynamic-forms-resolve';
@@ -14,7 +15,8 @@ import { DynamicFormsResolve } from '../shared/dynamic-forms/dynamic-forms-resol
         DynamicFormsResolve
     ],
     declarations: [
-        LogInComponent
+        LogInComponent,
+        TwoFactorLogInComponent
     ]
 })
 export class LogInModule { }
