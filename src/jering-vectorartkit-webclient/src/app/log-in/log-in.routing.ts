1376c5332817869232f14ac0777a176d56d42368
JeremyTan <jeremytanchongdao@gmail.com>
2016-11-21 18:40:23 +0800

    Added routing for twofactorlogin

--- src/jering-vectorartkit-webclient/src/app/log-in/log-in.routing.ts
+++ src/jering-vectorartkit-webclient/src/app/log-in/log-in.routing.ts
@@ -2,6 +2,7 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { LogInComponent } from './log-in.component';
+import { TwoFactorLogInComponent } from './two-factor-log-in/two-factor-log-in.component';
 import { DynamicFormsResolve } from '../shared/dynamic-forms/dynamic-forms-resolve';
 
 const logInRoutes: Routes = [
@@ -11,6 +12,17 @@ const logInRoutes: Routes = [
         data: {
             formModelName: LogInComponent.formModelName,
             formSubmitRelativeUrl: LogInComponent.formSubmitRelativeUrl
+        },
+        resolve: {
+            dynamicForm: DynamicFormsResolve
+        }
+    },
+    {
+        path: 'twofactorlogin',
+        component: TwoFactorLogInComponent,
+        data: {
+            formModelName: TwoFactorLogInComponent.formModelName,
+            formSubmitRelativeUrl: TwoFactorLogInComponent.formSubmitRelativeUrl
         },
         resolve: {
             dynamicForm: DynamicFormsResolve
