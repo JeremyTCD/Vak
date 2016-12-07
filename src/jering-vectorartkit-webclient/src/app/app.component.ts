fac49ff2e19fc2e0b2f04ebb92892021448285fa
JeremyTan <jeremytanchongdao@gmail.com>
2016-12-07 12:22:42 +0800

    AppComponent.logOff now sends http request to logOff

--- src/jering-vectorartkit-webclient/src/app/app.component.ts
+++ src/jering-vectorartkit-webclient/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { HttpService } from './shared/http.service';
 import { UserService } from './shared/user.service';
 
 @Component({
@@ -7,15 +9,27 @@ import { UserService } from './shared/user.service';
     templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+    private _logOffRelativeUrl = `Account/LogOff`;
     title = 'Vector Art Kit';
 
-    constructor(public userService: UserService) { }
+    constructor(public userService: UserService,
+        private _httpService: HttpService,
+        private _router: Router) { }
 
     ngOnInit(): void {
-        this.userService.syncWithStorage();
+        this.userService.init();
     }
 
     logOff(): void {
-        this.userService.logOff();
+        this.
+            _httpService.
+            post(this._logOffRelativeUrl, null).
+            subscribe(responseModel => {
+                this._router.navigate([`/home`]);
+            },
+            undefined,
+            () => {
+                this.userService.logOff();
+            });
     }
 }
