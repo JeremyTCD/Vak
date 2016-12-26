0df7dc1a9bcea4b7c1a6e4d713736c2551c1f447
JeremyTan <jeremytanchongdao@gmail.com>
2016-12-26 18:25:26 +0800

    Added firstCharToLowercase extension method for strings in jering.utilities

---
+++ src/Jering.Utilities/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Jering.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string firstCharToLowercase(this string s)
+        {
+            return char.ToLowerInvariant(s[0]) + s.Substring(1);
+        }
+    }
+}
