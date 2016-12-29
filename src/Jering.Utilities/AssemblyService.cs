72ff19f68f96c579913845b91c791347ccd18d56
JeremyTan <jeremytanchongdao@gmail.com>
2016-12-29 21:51:50 +0800

    Fleshed out Jering.Utilities.AssemblyService. Exposes a single function, GetReferencingAssemblies(string assemblyName). Caches referencing assemblies.

--- src/Jering.Utilities/AssemblyService.cs
+++ src/Jering.Utilities/AssemblyService.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.DependencyModel;
+using System.Linq;
+using System.Collections.Concurrent;
 
 namespace Jering.Utilities
 {
@@ -7,13 +11,57 @@ namespace Jering.Utilities
     /// </summary>
     public class AssemblyService : IAssemblyService
     {
+        private ConcurrentDictionary<string, IEnumerable<Assembly>> _referencingAssemblies { get; }
+
+        /// <summary>
+        /// Constructs a new instance of <see cref="AssemblyService"/> .
+        /// </summary>
+        public AssemblyService()
+        {
+            _referencingAssemblies = new ConcurrentDictionary<string, IEnumerable<Assembly>>();
+        }
+
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="assemblyName"></param>
         /// <returns></returns>
-        public virtual Assembly GetEntryAssembly()
+        public virtual IEnumerable<Assembly> GetReferencingAssemblies(string assemblyName)
         {
-            return Assembly.GetEntryAssembly();
-        } 
+            return _referencingAssemblies.GetOrAdd(assemblyName, ReferencingAssembliesGenerator);
+        }
+
+        #region Helpers
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assemblyName"></param>
+        /// <returns></returns>
+        public IEnumerable<Assembly> ReferencingAssembliesGenerator(string assemblyName)
+        {
+            List<Assembly> assemblies = new List<Assembly>();
+            IReadOnlyList<RuntimeLibrary> dependencies = DependencyContext.Default.RuntimeLibraries;
+            foreach (RuntimeLibrary library in dependencies)
+            {
+                if (IsCandidateLibrary(library, assemblyName))
+                {
+                    Assembly assembly = Assembly.Load(new AssemblyName(library.Name));
+                    assemblies.Add(assembly);
+                }
+            }
+            return assemblies;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="library"></param>
+        /// <param name="assemblyName"></param>
+        /// <returns></returns>
+        public bool IsCandidateLibrary(RuntimeLibrary library, string assemblyName)
+        {
+            return library.Dependencies.Any(d => d.Name.StartsWith(assemblyName));
+        }
+        #endregion
     }
 }
--- src/Jering.Utilities/IAssemblyService.cs
+++ src/Jering.Utilities/IAssemblyService.cs
@@ -1,9 +1,11 @@
-﻿using System.Reflection;
+﻿using Microsoft.Extensions.DependencyModel;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Jering.Utilities
 {
     public interface IAssemblyService
     {
-        Assembly GetEntryAssembly();
+        IEnumerable<Assembly> GetReferencingAssemblies(string assemblyName);
     }
 }
