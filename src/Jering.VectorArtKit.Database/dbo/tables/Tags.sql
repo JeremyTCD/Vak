bd3fe9ddfc8c4e964cfb5490c455a41d6c370426
JeremyTan <jeremytanchongdao@gmail.com>
2017-01-27 17:17:38 +0800

    Added rowversion columns to Tags and VakUnits

--- src/Jering.VectorArtKit.Database/dbo/tables/Tags.sql
+++ src/Jering.VectorArtKit.Database/dbo/tables/Tags.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Tags] (
     [Value] NVARCHAR (256) NOT NULL,
     [TagId] INT            NOT NULL,
+    [RowVersion] ROWVERSION NULL, 
     CONSTRAINT [PK_Tags] PRIMARY KEY CLUSTERED ([TagId] ASC)
 );
 
--- src/Jering.VectorArtKit.Database/dbo/tables/VakUnits.sql
+++ src/Jering.VectorArtKit.Database/dbo/tables/VakUnits.sql
@@ -2,6 +2,7 @@
     [VakUnitId] INT            IDENTITY (1, 1) NOT NULL,
     [Name]      NVARCHAR (256) NOT NULL,
     [AccountId] INT            NOT NULL,
+    [RowVersion] ROWVERSION NOT NULL, 
     CONSTRAINT [PK_VakUnits] PRIMARY KEY CLUSTERED ([VakUnitId] ASC),
     CONSTRAINT [FK_dbo_VakUnits_dbo_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Accounts] ([AccountId]) ON DELETE CASCADE
 );
