dbeb8a20af0f8c172ae8f67936cfdf0d53d34f16
JeremyTan <jeremytanchongdao@gmail.com>
2017-02-04 16:05:51 +0800

    Added ufnToRawJsonArray user function to Jering.VectorArtKitDatabase. Fixed Tags table TagId column to be an identity column.

--- src/Jering.VectorArtKit.Database/dbo/tables/Tags.sql
+++ src/Jering.VectorArtKit.Database/dbo/tables/Tags.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Tags] (
     [Value] NVARCHAR (256) NOT NULL,
-    [TagId] INT            NOT NULL,
+    [TagId] INT IDENTITY (1, 1) NOT NULL,
     [RowVersion] ROWVERSION NULL, 
     CONSTRAINT [PK_Tags] PRIMARY KEY CLUSTERED ([TagId] ASC)
 );
